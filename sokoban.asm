178620c153ee19fd6597e563f48a54ffd617ba0f
kyumin17 <kyumin2805@gmail.com>
2026-01-22 21:43:52 +0900

    Feat: draw map

--- sokoban.asm
+++ sokoban.asm
@@ -4,7 +4,10 @@ section .data
     RESET db 27, "[0m"
     STYLE_LEN equ $ - RESET
     REMOVE_CURSOR db 27, "[?25l"
-    CLEAR db 27, "[2J"
+    TOTAL_RESET db 27, "[H", 27, "[?25h"
+    TOTAL_RESET_LEN equ $ - TOTAL_RESET
+    CLEAR db 27, "[2J", 27, "[3J", 27, "[H"
+    CLEAR_LEN equ $ - CLEAR
 
     ;move
     MOVE db 27, "["
@@ -25,15 +28,26 @@ section .data
     ARROW_LEN equ $ - ARROW
     SPACE db " ", 0
     SPACE_LEN equ $ - SPACE
-
-    PLAYER db "i", 0
+    PLAYER db 27, "[38;5;15m", 0xE2, 0x97, 0x8F, " ", 27, "[0m", 0
     PLAYER_LEN equ $ - PLAYER
+    WALL db 27, "[48;5;245m", "  ", 27, "[0m", 0
+    WALL_LEN equ $ - WALL
+    GROUND db 27, "  ", 0
+    GROUND_LEN equ $ - GROUND
+    BOX db 27, "[38;5;130m", 0xE2, 0x98, 0x92, " ", 27, "[0m", 0
+    BOX_LEN equ $ - BOX
+    TARGET db 27, "[38;5;14m", 0xE2, 0x97, 0x86, " ", 27, "[0m", 0
+    TARGET_LEN equ $ - TARGET
 
     ;setting
-    WIDTH equ 50
+    WIDTH equ 25
     HEIGHT equ 10
 
+    ;map
+    FILE_NAME db "map/map.bin", 0
+
 section .bss
+    map: resb 250
     menu: resb 1
     key: resb 3
     input: resb 1
@@ -55,6 +69,7 @@ main:
 start_page:
     push rbp
     mov rbp, rsp
+    call remove_cursor
     call clear_page
     mov byte [menu], 0
 
@@ -126,30 +141,29 @@ start_page:
     call game_page
 
 .exit:
+    call total_reset
     leave
     ret
 
 game_page:
     push rbp
     mov rbp, rsp
-    mov dword [x], 27
-    mov dword [y], 6
+    call load_map
 
 .play:
-    mov edi, [x]
-    mov esi, [y]
-    mov rdx, PLAYER
-    mov rcx, PLAYER_LEN
-    call pos_write
+    call clear_page
+
+    mov eax, [y]
+    imul eax, WIDTH
+    add eax, [x]
+
+    mov byte [map + eax], 2
+    push rax
+    call draw_map
+    pop rax
+    mov byte [map + eax], 0
 
     call read_key
-
-    mov edi, [x]
-    mov esi, [y]
-    mov rdx, SPACE
-    mov rcx, SPACE_LEN
-    call pos_write
-    
     mov dl, [input]
     cmp dl, 0
     je .up
@@ -159,26 +173,20 @@ game_page:
     je .right
     cmp dl, 3
     je .left
+    cmp dl, 5
+    je .exit
     jmp .play
 
 .up:
-    cmp dword [y], 1
-    je .play
     dec dword [y]
     jmp .play
 .down:
-    cmp dword [y], HEIGHT
-    je .play
     inc dword [y]
     jmp .play
 .right:
-    cmp dword [x], WIDTH
-    je .play
     inc dword [x]
     jmp .play
 .left:
-    cmp dword [x], 1
-    je .play
     dec dword [x]
     jmp .play
 
@@ -215,6 +223,9 @@ int_to_str: ;(x: int) -> string
 pos_write: ;(x: int, y: int, msg: str, len: int) -> void
     push rbp
     mov rbp, rsp
+    push r12
+    push r13
+    push r14
     mov r12, rsi
     mov r13, rdx
     mov r14, rcx
@@ -235,6 +246,9 @@ pos_write: ;(x: int, y: int, msg: str, len: int) -> void
     mov rdi, r13
     mov rsi, r14
     call write
+    pop r12
+    pop r13
+    pop r14
     leave
     ret
 
@@ -249,6 +263,10 @@ read_key: ;() -> input
 
     cmp byte [key], 10 ;enter
     mov byte [input], 4
+    je .exit
+
+    cmp byte [key], "q" ;q
+    mov byte [input], 5
     je .exit
 
     mov byte [input], -1
@@ -268,6 +286,99 @@ read_key: ;() -> input
     mov byte [input], 3
 
 .exit:
+    leave
+    ret
+
+;;;;;util;;;;;
+draw_map:
+    push rbp
+    mov rbp, rsp
+    push r12
+    mov r12, 0
+
+.loop:
+    lea rax, [map + r12]
+
+    push rax
+
+    xor rdx, rdx
+    mov rax, r12
+    mov rbx, WIDTH
+    div rbx
+
+    mov rdi, rdx ;x
+    imul rdi, 2
+    mov rsi, rax ;y
+    inc rdi
+    inc rsi
+
+    pop rax
+    cmp byte [rax], 1 ;target
+    mov rdx, TARGET
+    mov rcx, TARGET_LEN
+    je .draw
+    cmp byte [rax], 2 ;player
+    mov rdx, PLAYER
+    mov rcx, PLAYER_LEN
+    je .draw
+    cmp byte [rax], 3 ;box
+    mov rdx, BOX
+    mov rcx, BOX_LEN
+    je .draw
+    cmp byte [rax], 4 ;wall
+    mov rdx, WALL
+    mov rcx, WALL_LEN
+    je .draw
+    cmp byte [rax], 0
+    mov rdx, GROUND
+    mov rcx, GROUND_LEN
+    je .draw
+    jmp .skip_draw
+
+.draw:
+    call pos_write
+.skip_draw:
+    inc r12
+    cmp r12, 250
+    jl .loop
+    pop r12
+    leave
+    ret
+
+load_map:
+    push rbp
+    mov rbp, rsp
+
+    mov rax, 2
+    mov rdi, FILE_NAME
+    mov rsi, 0
+    mov rdx, 0
+    syscall
+
+    mov rbx, rax
+
+    mov rax, 0
+    mov rdi, rbx
+    mov rsi, x
+    mov rdx, 1
+    syscall
+
+    mov rax, 0
+    mov rdi, rbx
+    mov rsi, y
+    mov rdx, 1
+    syscall
+
+    mov rax, 0
+    mov rdi, rbx
+    mov rsi, map
+    mov rdx, 250
+    syscall
+
+    mov rax, 3
+    mov rdi, rbx
+    syscall
+
     leave
     ret
 
@@ -294,7 +405,25 @@ clear_page:
     push rbp
     mov rbp, rsp
     mov rdi, CLEAR
-    mov rsi, STYLE_LEN
+    mov rsi, CLEAR_LEN
+    call write
+    leave
+    ret
+
+remove_cursor:
+    push rbp
+    mov rbp, rsp
+    mov rdi, REMOVE_CURSOR
+    lea rsi, [STYLE_LEN + 2]
+    call write
+    leave
+    ret
+
+total_reset:
+    push rbp
+    mov rbp, rsp
+    mov rdi, TOTAL_RESET
+    lea rsi, [TOTAL_RESET_LEN + 2]
     call write
     leave
     ret
